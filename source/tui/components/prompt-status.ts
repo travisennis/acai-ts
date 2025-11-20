d70790517bda26f06de795c224644a46ea6ced44
Travis Ennis <travis@travisennis.com>
2025-11-19 20:22:17 -0500

    feat(ui): update layout of prompt-status component

--- source/tui/components/prompt-status.ts
+++ source/tui/components/prompt-status.ts
@@ -1,17 +1,19 @@
-import { displayProgressBar, hr } from "../../terminal/index.ts";
+import type { ModelManager } from "../../models/manager.ts";
+import { displayProgressBar } from "../../terminal/index.ts";
 import style from "../../terminal/style.ts";
 import type { Component } from "../index.ts";
 
 type State = {
-  modelId: string;
   projectStatus: string;
   currentContextWindow: number;
   contextWindow: number;
 };
 
 export class PromptStatusComponent implements Component {
+  private modelManager: ModelManager;
   private state: State;
-  constructor(state: State) {
+  constructor(modelManager: ModelManager, state: State) {
+    this.modelManager = modelManager;
     this.state = state;
   }
 
@@ -21,9 +23,13 @@ export class PromptStatusComponent implements Component {
 
   render(width: number): string[] {
     const results: string[] = [];
-    results.push(style.dim(hr(width)));
+    // results.push(style.dim(hr(width)));
     results.push(this.state.projectStatus);
-    results.push(style.dim(this.state.modelId));
+    results.push(
+      style.dim(
+        `${this.modelManager.getModelMetadata("repl").id} [${this.modelManager.getModel("repl").modelId}]`,
+      ),
+    );
     results.push(
       displayProgressBar(
         this.state.currentContextWindow,
