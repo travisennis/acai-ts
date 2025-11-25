7d0b576d0be5ca430a0da3172bdcdde040d732f2
Travis Ennis <travis@travisennis.com>
2025-11-24 23:20:57 -0500

    test(tools): update import for consumeToolAsyncIterable
    - Change import path from manual-loop.ts to async-iterable.ts
    - Maintain test functionality with new utility location

--- test/tools/dynamic-tool-integration.test.ts
+++ test/tools/dynamic-tool-integration.test.ts
@@ -2,7 +2,7 @@ import assert from "node:assert/strict";
 import fs from "node:fs";
 import path from "node:path";
 import test from "node:test";
-import { consumeToolAsyncIterable } from "../../source/agent/manual-loop.ts";
+import { consumeToolAsyncIterable } from "../../source/tools/async-iterable.ts";
 import { loadDynamicTools } from "../../source/tools/dynamic-tool-loader.ts";
 
 test("Dynamic tool integration - end-to-end functionality", async () => {
